694e52f0f05bcdd6daf1a89b71820fc5f85b6d9e
TheMrTiky <95758136+TheMrTiky@users.noreply.github.com>
2021-12-13 23:48:11 +0000

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -59,7 +59,10 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation',
-		'Ball'
+		'Ball',
+		'Instakill Note',
+		'Health 0.5 Note',
+		'Health Note'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
